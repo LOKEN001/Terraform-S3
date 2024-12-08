595009810cd18c49c718393e8dcf579e2f12f5e1
LOKEN001 <lokendraprajapati354@gmail.com>
2024-12-08 10:41:03 +0530

    adding bucket ownership and access to public

--- main.tf
+++ main.tf
@@ -1,3 +1,32 @@
 resource "aws_s3_bucket" "myS3bucket" {
   bucket = var.bucketname
 }
+
+## bucket ownership
+resource "aws_s3_bucket_ownership_controls" "bucketOwn" {
+  bucket = aws_s3_bucket.myS3bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+## bucket public
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.myS3bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucketOwn,
+    aws_s3_bucket_public_access_block.public,
+  ]
+
+  bucket = aws_s3_bucket.myS3bucket.id
+  acl    = "public-read"
+}
